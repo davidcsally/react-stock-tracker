08c7c8ee82988b1bdcb704d6d3c251c3b372ae4a
David Sally <davidcsally@gmail.com>
2018-04-24 18:08:55 -0700

    (add) initial state

---
+++ src/reducers/initialState.js
@@ -0,0 +1,26 @@
+export default {
+  stocks: [
+    // {
+    //   ticker: '',
+    //   price: 0,
+    //   volume: 0,
+    //   timestamp: '',
+    //   holdings: 0,
+    //   totalShares: 0,
+    //   totalValue: 0,
+    //   return: 0,
+    //   buys: [
+    //     {
+    //       ticker: '',
+    //       shares: 0,
+    //       purPrice: 0,
+    //       date: '',
+    //     },
+    //   ],
+    // },
+  ],
+  tickers: [],
+  portfolioValue: 0,
+  portfolioReturn: 0,
+  view: 'home'
+};
