c67d335d0de4f2e498566e66664828a01a12db58
David Sally <davidcsally@gmail.com>
2018-04-24 18:08:03 -0700

    (add) app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import store from '../../reducers';
+import { updateView } from '../../actionCreators';
+
+import StockList from '../StockList/StockList';
+import HomePage from '../../views/HomePage/HomePage';
+import AddTrades from '../../views/AddTrades/AddTrades';
 import './App.scss';
 
-export default () => (
-  <div styleName="container">
-    {/* your app here */}
-  </div>
-);
+class App extends Component {
+  renderView = () => {
+    const { view } = this.props;
+    if (view === 'add') return <AddTrades />
+    return <HomePage />
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderView()}
+        {/* <BrowserRouter>
+        <Switch>
+          <Route path="/" component={HomePage} />
+          <Route path="/add" component={AddTrades} />
+        </Switch>
+      </BrowserRouter> */}
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ view: state.view }),
+  { updateView },
+)(App);
